39126777ef87acae7928fa55c7273e5052de4319
Valerie Walsh <35744819+vwalsh86@users.noreply.github.com>
2018-04-22 15:24:41 +0100

    Edited code
    Created a print request to print all together as an alternative to individually

--- max.py
+++ max.py
@@ -1,7 +1,7 @@
-# Valerie Walsh 2018-04-22
-# Iris Flower Data Set
-
-#Max of each column
+# Valerie Walsh 22-04-2018
+# Iris Data Set project
+# Reference: Topic 7 videos
+# Max of each column
 
 import numpy
 data = numpy.genfromtxt('iris.csv', delimiter=',')
@@ -11,6 +11,8 @@ maxsecondcol = numpy.max(data[:,1])
 maxthirdcol = numpy.max(data[:,2])
 maxforthcol = numpy.max(data[:,3])
 
+print('\n')
+
 print("First column max is:", maxfirstcol)
 
 print("Second column max is:", maxsecondcol)
@@ -18,3 +20,11 @@ print("Second column max is:", maxsecondcol)
 print("Third column max is:", maxthirdcol)
 
 print("Forth column max is:", maxforthcol)
+
+print('\n')
+
+#Or alternatively you could request a print out of all the columns such as;
+
+print("The max of each column is:", maxfirstcol, maxsecondcol, maxthirdcol, maxforthcol)
+
+print('\n')
