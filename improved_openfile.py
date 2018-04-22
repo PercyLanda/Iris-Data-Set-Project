061451ad551959607cfbb18ac129e713d2a1d7c7
Valerie Walsh <35744819+vwalsh86@users.noreply.github.com>
2018-04-22 15:16:21 +0100

    Editing comments

--- improved_openfile.py
+++ improved_openfile.py
@@ -1,6 +1,7 @@
-# Valerie Walsh
-# Content for this file taken from https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
-# Load dataset, clearer looking output to Week 5 attempt 'openfile.py'
+# Valerie Walsh 10-04-2018
+# Iris Data Set project
+# Reference: https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+# Output the dataset, clearer looking output to Week 5 attempt 'openfile.py'
 
 import pandas
 open = "iris.csv"
