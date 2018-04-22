5d688f02f3010dd8a8f0cdc240431c44705f4a73
Valerie Walsh <35744819+vwalsh86@users.noreply.github.com>
2018-04-22 15:25:45 +0100

    Editing the code of the file
    Created a print request to print all together as an alternative to individually

--- min.py
+++ min.py
@@ -1,7 +1,7 @@
 # Valerie Walsh 2018-04-22
-# Iris Flower Data Set
-
-#Min of each column
+# Iris Data Set project
+# Reference: Topic 7 videos
+# Calculate the min of each column
 
 import numpy
 data = numpy.genfromtxt('iris.csv', delimiter=',')
@@ -11,6 +11,8 @@ minsecondcol = numpy.min(data[:,1])
 minthirdcol = numpy.min(data[:,2])
 minforthcol = numpy.min(data[:,3])
 
+print('\n')
+
 print("First column min is:", minfirstcol)
 
 print("Second column min is:", minsecondcol)
@@ -18,3 +20,11 @@ print("Second column min is:", minsecondcol)
 print("Third column min is:", minthirdcol)
 
 print("Forth column min is:", minforthcol)
+
+print('\n')
+
+#Or alternatively you could request a print out of all the columns such as;
+
+print("The min of each column is:", minfirstcol, minsecondcol, minthirdcol, minforthcol)
+
+print('\n')
