454d75714b6fe7ca4a2f619fb7eeaa63ab27cfa7
Valerie Walsh <35744819+vwalsh86@users.noreply.github.com>
2018-04-22 15:10:15 +0100

    Adding comments to script

--- box_whisker.py
+++ box_whisker.py
@@ -1,17 +1,13 @@
-# Load libraries
+# Valerie Walsh 22-04-2018
+# Iris Data Set Project - scripts
+# Reference: https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+
+# Code for Box and Whisker Plots of Iris Data Set 
+
+# Load libraries required 
 import pandas
 from pandas.plotting import scatter_matrix
-import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+import matplotlib.pyplot as p
 
 # Load dataset
 url = "iris.csv"
@@ -20,4 +16,4 @@ dataset = pandas.read_csv(url, names=names)
 
 # box and whisker plots
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-plt.show()
+p.show()
