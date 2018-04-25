747f9635202eda9a7a020e4920f6e49ada44b14d
Valerie Walsh <35744819+vwalsh86@users.noreply.github.com>
2018-04-25 09:35:37 +0100

    Uploaded scripts
    3D and Validation 
    Was unable to complete the validation file output successfully however I did want to include it to show attempts and work to date, I hope to revisit and complete it in the future

---
+++ validation.py
@@ -0,0 +1,41 @@
+# Valerie Walsh 25/04/2018
+# Iris Flower Data Set project
+# Creating a validation dataset to test the data
+import pandas
+open = "iris.csv"
+names = ['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width', 'Class']
+model_selection = pandas.read_csv(open, names=names)
+
+# Split-out validation dataset
+array = model_selection.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
+
+
+
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
